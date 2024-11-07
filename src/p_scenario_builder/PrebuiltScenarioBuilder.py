074a55a9c718ef0b6546c7d7261f32afada600bd
Mel Chafart <141696933+mchaf0126@users.noreply.github.com>
2024-11-06 21:17:08 -0800

    Create placeholder for prebuilt scenarios in code

---
+++ src/p_scenario_builder/PrebuiltScenarioBuilder.py
@@ -0,0 +1,44 @@
+# from pathlib import Path
+from dataclasses import dataclass
+# import pandas as pd
+# import src.utils.general as gen
+
+
+@dataclass
+class PrebuiltScenarioBuilder:
+    """Methods for creating a prebuilt scenario"""
+    # attributes
+
+    # methods
+
+
+@dataclass
+class TransportationScenarioBuilder(PrebuiltScenarioBuilder):
+    """Methods for creating a prebuilt scenario"""
+    # attributes
+
+    # methods
+
+
+@dataclass
+class ConstructionScenarioBuilder(PrebuiltScenarioBuilder):
+    """Methods for creating a prebuilt scenario"""
+    # attributes
+
+    # methods
+
+
+@dataclass
+class ReplacementScenarioBuilder(PrebuiltScenarioBuilder):
+    """Methods for creating a prebuilt scenario"""
+    # attributes
+
+    # methods
+
+
+@dataclass
+class EndOfLifeScenarioBuilder(PrebuiltScenarioBuilder):
+    """Methods for creating a prebuilt scenario"""
+    # attributes
+
+    # methods
---
+++ src/p_scenario_builder/build_prebuilt_scenarios.py
@@ -0,0 +1,12 @@
+# from pathlib import Path
+# import pandas as pd
+# import src.utils.general as gen
+
+
+def build_prebuilt_scenarios():
+    """placeholder for implementation of prebuilt scenario"""
+    # implement prebuilt scenario classes as appropriate
+
+
+if __name__ == '__main__':
+    build_prebuilt_scenarios()
